e3027d66449498560e18cd53d3bc3812f16da3b3
Alvaro Augusto do Nascimento Filho <alvarosum@hotmail.com>
2024-02-14 19:31:47 -0300

    refactor.config route create user and controller

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+
+const useRoute = require("./src/routes/userRouter");
+const PORT = 3333;
+
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("hello world")
-})
 
-app.listen(3333)
+app.use(express.json())
+
+app.use("/user", useRoute);
+
+app.listen(PORT, () => console.log(`Servidor na porta ${PORT}`));
---
+++ src/routes/userRouter.js
@@ -0,0 +1,6 @@
+const route = require("express").Router();
+const userController = require("../controllers/userController");
+
+route.post("/", userController.create);
+
+module.exports = route;
