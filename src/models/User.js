cb6b50d210a0825deb586033a1351c7202c19cb5
Alvaro Augusto do Nascimento Filho <alvarosum@hotmail.com>
2024-02-21 23:13:34 -0300

    feat: creat news schemas

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt"
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true, select: false },
   avatar: { type: String, required: true },
   background: { type: String, required: true },
