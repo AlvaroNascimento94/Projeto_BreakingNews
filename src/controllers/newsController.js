f4626c1d775cdda2ac51ab3e2319ed39a204d83f
Alvaro Augusto do Nascimento Filho <alvarosum@hotmail.com>
2024-03-03 16:08:25 -0300

    URL pagination

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -1,4 +1,8 @@
-import { createService, findAllService } from "../services/newsServices.js";
+import {
+  createService,
+  findAllService,
+  countNews,
+} from "../services/newsServices.js";
 
 const create = async (req, res) => {
   try {
@@ -21,11 +25,54 @@ const create = async (req, res) => {
   }
 };
 const findAll = async (req, res) => {
-  const news = await findAllService();
+  let { limit, offset } = req.query;
+
+  limit = Number(limit);
+  offset = Number(offset);
+
+  if (!limit) {
+    limit = 5;
+  }
+
+  if (!offset) {
+    offset = 0;
+  }
+
+  const news = await findAllService(offset, limit);
+  const total = await countNews();
+  const currentURL = req.baseUrl;
+
+  const next = offset + limit;
+  const nextURL =
+    next < total ? `${currentURL}?limit=${limit}&offset=${next}` : null;
+
+  const previous = offset - limit < 0 ? null : offset - limit;
+  const previousURL =
+    previous !== null
+      ? `${currentURL}?limit=${limit}&offset=${previous}`
+      : null;
+
   if (news.length === 0) {
     return res.status(400).send("There are no registered news");
   }
-  res.send(news);
+  res.send({
+    nextURL,
+    previousURL,
+    limit,
+    offset,
+    total,
+    results: news.map((itens) => ({
+      id: itens._id,
+      title: itens.title,
+      text: itens.text,
+      banner: itens.banner,
+      likes: itens.likes,
+      comments: itens.comments,
+      name: itens.user.name,
+      username: itens.user.username,
+      userAvatar: itens.user.avatar,
+    })),
+  });
 };
 
 export { create, findAll };
