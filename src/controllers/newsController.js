4c0cbec49ee46d2d0596ba2108a13947823a1c12
Alvaro Augusto do Nascimento Filho <alvarosum@hotmail.com>
2024-03-03 17:02:48 -0300

    find by id news

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -3,9 +3,10 @@ import {
   findAllService,
   countNews,
   topNewsService,
+  findByIdService
 } from "../services/newsServices.js";
 
-const create = async (req, res) => {
+export const create = async (req, res) => {
   try {
     const { title, text, banner } = req.body;
 
@@ -25,7 +26,7 @@ const create = async (req, res) => {
     res.status(500).send({ message: error.message });
   }
 };
-const findAll = async (req, res) => {
+export const findAll = async (req, res) => {
   try {
     let { limit, offset } = req.query;
 
@@ -80,7 +81,7 @@ const findAll = async (req, res) => {
   }
 };
 
-const topNews = async (req, res) => {
+export const topNews = async (req, res) => {
   try {
     const news = await topNewsService();
     if (!news) {
@@ -105,4 +106,26 @@ const topNews = async (req, res) => {
   }
 };
 
-export { create, findAll, topNews };
+export const findById = async (req, res) =>{
+try {
+  const {id} = req.params
+  
+  const news = await findByIdService(id)
+
+  return res.send({
+    news: {
+      id: news._id,
+      title: news.title,
+      text: news.text,
+      banner: news.banner,
+      likes: news.likes,
+      comments: news.comments,
+      name: news.user.name,
+      username: news.user.username,
+      userAvatar: news.user.avatar,
+    },
+  })
+} catch (error) {
+  res.status(500).send({ message: error.message });
+}
+}
--- src/routes/newsRouter.js
+++ src/routes/newsRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { create, findAll, topNews} from "../controllers/newsController.js";
+import { create, findAll, topNews, findById} from "../controllers/newsController.js";
 import { authMiddleware } from "../middlewares/authMiddlewares.js";
 
 const router = Router();
@@ -7,5 +7,6 @@ const router = Router();
 router.post("/", authMiddleware, create);
 router.get("/", findAll);
 router.get("/top", topNews)
+router.get("/:id", findById)
 
 export default router;
