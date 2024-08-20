4866587c6f20d3b37b45eb63d01543ed5821a199
AlvaroNascimento94 <alvarosum@hotmail.com>
2024-08-20 00:50:48 -0300

    #

--- BackEnd/src/services/authServices.js
+++ BackEnd/src/services/authServices.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
@@ -10,16 +9,3 @@ const generateToken = (id) =>
   jwt.sign({ id: id }, process.env.SECRET, { expiresIn: 86400 });
 
 export { loginService, generateToken };
-=======
-import User from "../models/User.js";
-import jwt from "jsonwebtoken";
-
-const loginService = (email) => {
-  return User.findOne({ email: email }).select("+password");
-};
-
-const generateToken = (id) =>
-  jwt.sign({ id: id }, process.env.SECRET, { expiresIn: 86400 });
-
-export { loginService, generateToken };
->>>>>>> fb4a0ca7ec53b1c6ad947560f590137ca102458e
--- BackEnd/src/services/userServices.js
+++ BackEnd/src/services/userServices.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import User from "../models/User.js";
 
 const createService = (body) => User.create(body);
@@ -27,33 +26,3 @@ export default {
   findIdService,
   updateService
 };
-=======
-import User from "../models/User.js";
-
-const createService = (body) => User.create(body);
-
-const findAllService = () => User.find();
-
-const findIdService = (id) => User.findById(id);
-
-const updateService = (
-  id,
-  name,
-  username,
-  email,
-  password,
-  avatar,
-  background
-) =>
-  User.findByIdAndUpdate(
-    { _id: id }, 
-    { name, username, email, password, avatar, background }
-  );
-
-export default {
-  createService,
-  findAllService,
-  findIdService,
-  updateService
-};
->>>>>>> fb4a0ca7ec53b1c6ad947560f590137ca102458e
