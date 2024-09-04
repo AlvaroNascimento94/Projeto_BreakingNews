9beec8695dd0c1d1a23488b2ab7d2857d711cbb2
AlvaroNascimento94 <alvarosum@hotmail.com>
2024-09-04 19:41:19 -0300

    page search

--- FrontEnd/src/pages/Search/Search.jsx
+++ FrontEnd/src/pages/Search/Search.jsx
@@ -1,11 +1,52 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { searchNews } from "../../services/postServices";
+import { Card } from "../../components/Card/Card";
+import { ContainerResults, SearchNews, TestResults } from "./SearchStyled";
 
 export function Search() {
   const { title } = useParams();
+  const [post, setPost] = useState([]);
+
+  async function search() {
+    try {
+      const postsApi = await searchNews(title);
+      setPost(postsApi.data.foundNews);
+      console.log(postsApi.data.foundNews);
+    } catch (err) {
+      console.log(err);
+      setPost([]);
+    }
+  }
+  useEffect(() => {
+    search();
+  }, [title]);
 
   return (
-    <>
-      <h1>{title}</h1>
-    </>
+    <ContainerResults>
+      <TestResults>
+        <span>
+          {post.length !== 0
+            ? `Encontramos ${post.length} ${
+                post.length > 1 ? "resultados" : "resultado"
+              } para:`
+            : "Não encontramos reultados para"}
+        </span>
+        <h2>{title}</h2>
+      </TestResults>
+
+      <SearchNews>
+        {post.map((item) => (
+          <Card
+            key={item.id}
+            title={item.title}
+            text={item.text}
+            banner={item.banner}
+            likes={item.likes}
+            comments={item.comments}
+          />
+        ))}
+      </SearchNews>
+    </ContainerResults>
   );
 }
--- FrontEnd/src/services/postServices.js
+++ FrontEnd/src/services/postServices.js
@@ -10,3 +10,8 @@ export function getTopNews(){
     const response = axios.get(`${baseURL}/news/top`);
     return response
 }
+
+export function searchNews(title){
+    const response = axios.get(`${baseURL}/news/search?title=${title}`);
+    return response
+}
